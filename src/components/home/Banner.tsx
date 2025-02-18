9d04bf5bbd9499da45869af09a09cd53b2bd5a74
Siwakorn.Phum <150316488+siwakorn1123@users.noreply.github.com>
2025-02-18 12:45:16 +0700

    bannerggg

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -15,18 +15,18 @@ export const Banner = () => {
       {/* เนื้อหา */}
       <div className="relative z-10 flex flex-col items-center justify-center py-20 px-4 text-center lg:py-40 lg:px-8">
         <h1 className="text-4xl font-bold mb-4 lg:text-6xl">
-          โทรศัพท์มือถือที่ดีที่สุดแห่งปี 2024
+          หนังสืออิเล็กทรอนิกส์ที่ดีที่สุดแห่งปี 2024
 				</h1>
 
         <p className="text-lg mb-8 lg:text-2xl">
-          ค้นพบข้อเสนอพิเศษและโทรศัพท์มือถือรุ่นใหม่ล่าสุด
+          ค้นพบข้อเสนอพิเศษและ E-Book ใหม่ล่าสุด
 				</p>
 
 				<Link
           to="/celulares"
           className="bg-gray-900 hover:bg-gray-950 text-white font-semibold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out"
 				>
-          ดูโทรศัพท์มือถือ
+          ดู E-Book
 				</Link>
 			</div>
 		</div>
